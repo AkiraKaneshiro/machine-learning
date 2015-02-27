38e58b7d2835ea41a63cc7c1a32df7072c442da9
Daniel Kronovet <kronovet@gmail.com>
2015-02-27 01:06:11 -0500

    implemented MLE_cov

--- core/classifiers.py
+++ core/classifiers.py
@@ -10,6 +10,7 @@ All functions currently operate on a pandas DataFrame or Series.
 import math
 
 import numpy as np
+import pandas as pd
 
 
 ######################################
@@ -17,10 +18,15 @@ import numpy as np
 ######################################
 def get_euclidean_distance(u, v):
     '''u, v are vectors of equal length'''
-    return math.sqrt(sum([(ui - vi) ** 2 for ui, vi in zip(u, v)]))
+    # return math.sqrt(sum([(ui - vi) ** 2 for ui, vi in zip(u, v)]))
+    return math.sqrt(((u - v) ** 2).sum())
+
+def get_ln_norm(u, n):
+    # return math.sqrt(sum([ui ** n for ui in u]))
+    return math.sqrt((u ** n).sum())
 
 def get_euclidean_norm(u):
-    return math.sqrt(sum([ui ** 2 for ui in u]))
+    return get_ln_norm(u, 2)
 
 def knn_classifier(X_train, label_train, x, k=3):
     centered_X = X_train - x
@@ -33,9 +39,35 @@ def knn_classifier(X_train, label_train, x, k=3):
     counts.sort()
     return counts[label].ix[0]
 
-def knn_classifier_short(X_train, label_train, x, k=3):
-    distances = (X_train - x).T.apply(get_euclidean_norm)
-    distances.sort()
-    counts = label_train.ix[distances.iloc[:k].index].groupby(0).count()
-    counts.sort()
-    return counts[0].ix[0]
+########################
+### Bayes Classifier ###
+########################
+def get_MLE_cov(sample):
+    mu = sample.mean()
+    n = len(sample.columns)
+    cov = pd.DataFrame(np.zeros(n**2).reshape(n,n))
+    for x in sample.index:
+        error = pd.DataFrame(sample.ix[x] - mu)
+        cov += (error.dot(error.T)) / n
+    return cov
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
